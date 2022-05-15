99a2e21ab51759e6afdc19b07e565dbf06014dfe
Sarvesh.Fegade <fegadesb19.comp@coep.ac.in>
2022-05-15 12:37:01 +0530

    added component todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstProject';
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.title = "Sarvesh";
+      
+  //   }, 4000);
+  // }
 }
