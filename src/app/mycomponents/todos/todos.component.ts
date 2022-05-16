ad2d5cab24570df7167d90d59aa0dbf7ac83c779
Sarvesh.Fegade <fegadesb19.comp@coep.ac.in>
2022-05-16 18:02:13 +0530

    todos add-todo todo-item components

--- src/app/mycomponents/todos/todos.component.ts
+++ src/app/mycomponents/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  todos:Todo[];
+
+  constructor() {
+    this.todos = [];
+   }
 
   ngOnInit(): void {
   }
 
+  deleteTodo(todo:Todo) {
+    console.log(todo);
+    console.log("dleteTodo is triggerd");
+    var index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
+
+  addTodo(todo: Todo) {
+    console.log(todo);
+    this.todos.push(todo);
+  }
 }
